2fa8ae4e379be9ca3dd8683d5dd0e70cd73da157
alex-kir <alex-kir@users.noreply.github.com>
2016-05-24 23:06:39 +0300

    demo updated;

--- src/AKCustomView.Demo/AKCustomView.Demo/AKCustomView.Demo.cs
+++ src/AKCustomView.Demo/AKCustomView.Demo/AKCustomView.Demo.cs
@@ -10,7 +10,8 @@ namespace AKCustomView.Demo
         {
 
             var content = new RelativeLayout();
-            content.Children.Add(new RoundBoxView()
+            RoundBoxView roundBox;
+            content.Children.Add(roundBox = new RoundBoxView()
                 {
                     BorderColor = Color.Green,
                     BorderWidth = 2,
@@ -22,6 +23,20 @@ namespace AKCustomView.Demo
                 Constraint.RelativeToParent(p => 200),
                 Constraint.RelativeToParent(p => 300));
 
+            content.Children.Add(new Button()
+            {
+                    Text = "Color",
+                Command = new Command(it => {
+                    double c = (DateTime.Now.Ticks % 1000) / 1000.0;
+                    MainPage.BackgroundColor = Color.FromRgb(c, c, c);
+                }),
+            },
+                Constraint.RelativeToParent(p => (p.Width - 200) / 2),
+                Constraint.RelativeToParent(p => (p.Height - 100)),
+                Constraint.RelativeToParent(p => 200),
+                Constraint.RelativeToParent(p => 50)
+            );
+           
             // The root page of your application
             MainPage = new ContentPage
             {
